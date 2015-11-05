b52f4588d2453f06bca0df378a1743195785a000
PerryMartin <perry.martin@ocsbstudent.ca>
2015-11-05 14:17:16 -0500

    v1.0

---
+++ volumeOfACylinder/Volume.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace volumeOfACylinder
+{
+    partial class frmVolume
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblRadius = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(13, 13);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(38, 13);
+            this.lblHeight.TabIndex = 0;
+            this.lblHeight.Text = "Height";
+            // 
+            // lblRadius
+            // 
+            this.lblRadius.AutoSize = true;
+            this.lblRadius.Location = new System.Drawing.Point(16, 46);
+            this.lblRadius.Name = "lblRadius";
+            this.lblRadius.Size = new System.Drawing.Size(40, 13);
+            this.lblRadius.TabIndex = 1;
+            this.lblRadius.Text = "Radius";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(81, 13);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 2;
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(81, 46);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 20);
+            this.txtRadius.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(19, 123);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(253, 23);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(71, 191);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 13);
+            this.lblAnswer.TabIndex = 5;
+            // 
+            // frmVolume
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.lblRadius);
+            this.Controls.Add(this.lblHeight);
+            this.Name = "frmVolume";
+            this.Text = "Volume";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblRadius;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ volumeOfACylinder/Volume.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Created by:  Perry Martin
+ * Created on: 05-11-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit#4-05
+ * This program the volume of a cylinder
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace volumeOfACylinder
+{
+    public partial class frmVolume : Form
+    {
+        public double CalculateVolume(double radius, double height)
+        {
+            // Variable
+
+            double volume = 0;
+
+            volume = Math.PI * Math.Pow(radius, 2) * height;
+
+            return volume;
+        }
+
+        public frmVolume()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // Variables
+            double radius;
+            double height;
+            double volume;
+
+            // Input
+            radius = Convert.ToDouble(this.txtRadius.Text);
+            height = Convert.ToDouble(this.txtHeight.Text);
+
+            // process
+
+            volume = CalculateVolume(radius, height);
+            volume = Math.Round(volume, 2);
+
+            
+            // output
+
+            this.lblAnswer.Text = Convert.ToString(volume);
+        
+         }
+    }
+}
